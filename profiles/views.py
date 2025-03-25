9f3187b81b84cabfa7a94beacd33a209c8dd95cc
mezter1337 <andreas.javert@gmail.com>
2025-03-25 14:06:27 +0100

    Fix further things like hiding album etc

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
-from mp_api.permissions import IsComposerOrOwner, IsOwnerOrReadOnly
+from mp_api.permissions import IsComposerOrOwner
 from .serializers import ProfileSerializer
 from .models import Profile
 
